70a5f448fc007a0a20043d8ef8488754dcfba5ea
Heather Hern <heathernewlin1@gmail.com>
2020-12-11 21:29:15 -0500

    Update db.js

--- public/db.js
+++ public/db.js
@@ -4,3 +4,13 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+
+
+
+
+request.onerror = function(event) {
+    console.log("error " + event.target.errorCode);
+};
